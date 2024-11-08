43f68aa5d331053f47bb5e667a5c0928627dffaf
Klameta <74143760+Klameta@users.noreply.github.com>
2024-11-08 11:46:35 +0200

    Adding the solution from today

---
+++ 01/01.01.FillingAndSortingAMatrix/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace _01._01.FillingAndSortingAMatrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[,] matrix = new int[4, 4];
+
+            FillMatrix(random, matrix);
+
+            OutputMatrix(matrix);
+
+            Console.WriteLine();
+            Console.WriteLine("-------");
+            Console.WriteLine();
+
+            SortMatrix(matrix);
+
+            OutputMatrix(matrix);
+        }
+
+        private static void FillMatrix(Random random, int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    int numberToInsert = random.Next(1, 6);
+                    matrix[i, j] = numberToInsert;
+                }
+            }
+        }
+
+        private static void SortMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j <= matrix.GetLength(1)-2; j++)
+                {
+                    for (int m = 0; m <= matrix.GetLength(1)-2; m++)
+                    {
+                        if (matrix[i, m] > matrix[i, m + 1])
+                        {
+                            int t = matrix[i, m + 1];
+                            matrix[i, m + 1] = matrix[i, m];
+                            matrix[i, m] = t;
+                        }
+                    }
+                }
+
+            }
+        }
+
+
+        private static void OutputMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
